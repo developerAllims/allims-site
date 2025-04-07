445a9a7760a2ee562702baeedadc0bf6f16f31a4
Ricardo Miranda <54359457+ricardomiranda-allims@users.noreply.github.com>
2025-04-07 10:33:08 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,49 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  plugins: [
+    react(),
+    tailwindcss(),
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+      exclude: undefined,
+      include: undefined,
+      includePublic: true,
+      logStats: true,
+      ansiColors: true,
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 100
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 100
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 100
+      },
+      tiff: {
+        // https://sharp.pixelplumbing.com/api-output#tiff
+        quality: 100
+      },
+      // gif does not support lossless compression
+      // https://sharp.pixelplumbing.com/api-output#gif
+      gif: {},
+      webp: {
+        // https://sharp.pixelplumbing.com/api-output#webp
+        lossless: true
+      },
+      avif: {
+        // https://sharp.pixelplumbing.com/api-output#avif
+        lossless: true
+      },
+      cache: false,
+      cacheLocation: undefined
+    })
+  ]
 })
