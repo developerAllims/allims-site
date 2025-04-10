bf939270860340d9f68a20e7ab0a2d24e331c568
Ricardo Miranda <54359457+ricardomiranda-allims@users.noreply.github.com>
2025-04-10 17:07:48 -0300

    responsive cookie

--- src/components/Policy/index.tsx
+++ src/components/Policy/index.tsx
@@ -12,20 +12,20 @@ export const Policy = () => {
   }, [setCookie])
   if (cookies[cookieName] === true) return <></>
   return (
-    <div className="flex justify-between items-center fixed left-1/10 bottom-12 min-h-16 w-8/10 p-3 gap-3 border-2 border-white rounded-md bg-gray-primary">
-      <div>
+    <div className="flex flex-col md:flex-row justify-between items-center fixed left-1/10 bottom-12 min-h-16 w-8/10 p-3 gap-3 border-2 border-white rounded-md bg-gray-primary">
+      <div className="flex">
         <ButtonLink
           title={name}
           to={docs[doc]}
           target="_blank"
-          className="bg-orange-primary text-white font-bold"
+          className="bg-orange-primary text-white font-bold text-center"
           showArrow={false}
         />
       </div>
-      <div>
-        <span className="text-white font-semibold">{text}</span>
+      <div className="flex">
+        <span className="text-white text-justify font-semibold">{text}</span>
       </div>
-      <div>
+      <div className="flex justify-end w-full md:w-fit">
         <ButtonSimple
           title={button}
           className="bg-orange-primary text-white font-bold"
